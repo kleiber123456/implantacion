50f49b07ebb858da4afc01669257b6f46ccf7fbd
kleiber123456 <arcilakleiber@gmail.com>
2025-05-17 18:15:41 -0500

    Se agrego todo el modulo de servicios

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 // src/app.js
 const express = require('express');
 const cors = require('cors');
+
 const authRoutes = require('./routes/authRoutes');
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const rolRoutes = require('./routes/rolRoutes');
 const proveedorRoutes = require('./routes/proveedorRoutes');
+const servicioRoutes = require('./routes/servicioRoutes');
 
 
 const app = express();
@@ -17,23 +19,32 @@ app.use('/api/auth', authRoutes);
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/roles', rolRoutes);
 app.use('/api/proveedores', proveedorRoutes);
+app.use('/api/servicios', servicioRoutes);
+
 
 
 app.get('/', (req, res) => {
     res.json({
         message: 'Bienvenido a la API del Taller MotOrtega',
         endpoints: {
-            roles: '/api/roles',
-            usuarios: '/api/usuarios',
             login: '/api/auth/login (POST)',
             register: '/api/auth/register (POST)',
-            miPerfil: '/api/usuarios/mi-perfil (GET/PUT)',
             enviarCodigo: '/api/auth/solicitar-codigo (POST)',
             verificarCodigo: '/api/auth/verificar-codigo (POST)',
             actualizarPassword: '/api/auth/nueva-password (POST)',
+
+            miPerfil: '/api/usuarios/mi-perfil (GET/PUT)',
+
+//-----------------------------------------------------------------------------
             cambiarEstadoUsuario: '/api/usuarios/:id/cambiar-estado (PUT)',
             cambiarEstadoProveedor: '/api/proveedores/:id/cambiar-estado (PUT)',
+            cambiarEstadoServicio: '/api/servicios/:id/cambiar-estado (PUT)',
+
             proveedores: '/api/proveedores',
+            servicios: '/api/servicios',
+            roles: '/api/roles',
+            usuarios: '/api/usuarios'
+            
             
         }
     });
