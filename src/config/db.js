f035bd796a2f355c1f2bfa26bd4f85d649a5d960
kleiber123456 <165842462+kleiber123456@users.noreply.github.com>
2025-09-08 12:25:31 -0500

    MELO CARAMELO

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,12 @@
-const { Pool } = require("pg")
-require("dotenv").config()
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, // Por defecto 5432
+});
 
-module.exports = pool
+module.exports = pool;
