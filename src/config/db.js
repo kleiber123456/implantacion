d5a8502fac992f319d7ae071d707e1deb0282a12
kleiber123456 <165842462+kleiber123456@users.noreply.github.com>
2025-09-08 14:19:47 -0500

    vamos

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg")
+require("dotenv").config()
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -7,11 +7,30 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-});
+})
 
 // A small check to confirm the pool is ready
-pool.on('connect', () => {
-  console.log('Client connected to the database');
-});
+pool.on("connect", () => {
+  console.log("Client connected to the database")
+})
 
-module.exports = pool;
+const connect = async () => {
+  const client = await pool.connect()
+
+  // Add PostgreSQL-compatible transaction methods
+  client.beginTransaction = async () => {
+    await client.query("BEGIN")
+  }
+
+  client.commit = async () => {
+    await client.query("COMMIT")
+  }
+
+  client.rollback = async () => {
+    await client.query("ROLLBACK")
+  }
+
+  return client
+}
+
+module.exports = { pool, connect }
